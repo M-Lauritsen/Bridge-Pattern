acbbf8f9d57d8339d6ddad532f808fb2c55bf97d
Michael Lauritsen <i30288741@live.dk>
2018-10-16 13:54:48 +0200

    Bridge Pattern Done

--- Bridge Pattern/Program.cs
+++ Bridge Pattern/Program.cs
@@ -8,8 +8,67 @@ namespace Bridge_Pattern
 {
     class Program
     {
+        public interface IOrderingSystem
+        {
+            void Place(string order);
+        }
+
+        public abstract class SendOrder
+        {
+            public IOrderingSystem _restaurant;
+
+            public abstract void Send();
+        }
+
+        public class SendDairyFreeOrder : SendOrder
+        {
+            public override void Send()
+            {
+                _restaurant.Place("Dairy-Free Order");
+            }
+        }
+
+        public class SendGlutenFreeOrder : SendOrder
+        {
+            public override void Send()
+            {
+                _restaurant.Place("Gluten-Free Order");
+            }
+        }
+
+        public class DinerOrders : IOrderingSystem
+        {
+            public void Place(string order)
+            {
+                Console.WriteLine("Placing order for " + order + " at the Diner.");
+            }
+        }
+
+        public class FancyRestaurantOrders : IOrderingSystem
+        {
+            public void Place (string order)
+            {
+                Console.WriteLine("Placing order for " + order + " at the Fancy Restaurant.");
+            }
+        }
+
         static void Main(string[] args)
         {
+            SendOrder _sendOrder = new SendDairyFreeOrder();
+            _sendOrder._restaurant = new DinerOrders();
+            _sendOrder.Send();
+
+            _sendOrder._restaurant = new FancyRestaurantOrders();
+            _sendOrder.Send();
+
+            _sendOrder = new SendGlutenFreeOrder();
+            _sendOrder._restaurant = new DinerOrders();
+            _sendOrder.Send();
+
+            _sendOrder._restaurant = new FancyRestaurantOrders();
+            _sendOrder.Send();
+
+            Console.ReadKey();
         }
     }
 }
